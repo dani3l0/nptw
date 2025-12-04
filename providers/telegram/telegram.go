88a71ca92d566e1a51dcd0d00e1a879368a77772
dani3l0 <daniel.jank@protonmail.com>
2025-12-04 01:00:15 +0100

    Autocache: no more sending message is required

--- providers/telegram/telegram.go
+++ providers/telegram/telegram.go
@@ -5,18 +5,22 @@ import (
 	"nptw/config"
 	"nptw/utils/log"
 	"os"
+	"time"
 
 	tg "github.com/amarnathcjd/gogram/telegram"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 var client *tg.Client
 var myID int64
 
+// Initializes Telegram connection
 func Init() {
 	client, _ = tg.NewClient(tg.ClientConfig{
 		AppID:    int32(config.Get().TelegramApiId),
 		AppHash:  config.Get().TelegramApiHash,
-		LogLevel: 1,
+		LogLevel: tg.WarnLevel,
 	})
 
 	err := client.ConnectBot(config.Get().TelegramBotToken)
@@ -26,15 +30,47 @@ func Init() {
 		os.Exit(1)
 	}
 
+	// Get info about running bot
 	myID = client.Me().ID
 	log.I(fmt.Sprint("Bot's ID is ", myID))
 
+	// Get info about target channels / refresh cache
+	initializeChannel(int64(config.Get().NotificationsChannelId), "notifications")
+	initializeChannel(int64(config.Get().ReplaysChannelId), "replays")
+
+	// Enable user-bot interaction
 	if config.Get().RespondToUserMessagesEnabled {
 		client.AddMessageHandler(tg.OnNewMessage, func(message *tg.NewMessage) error {
 			ReplyToUser(message)
 			return nil
 		})
 	}
-
 	log.I("Bot ready and running")
+}
+
+// Just a helper function that convers true/false into yes/no
+func initializeChannel(id int64, channelType string) {
+	log.I("Getting info about ", channelType, " channel")
+	for {
+		channel, err := client.GetChannel(id)
+		client.GetSendableChannel(id)
+		if !client.IdInCache(id) {
+			log.W("Please send a message to ", channelType, " channel")
+			time.Sleep(time.Second)
+		} else if err == nil {
+			postMessages := "no (REQUIRED!)"
+			if channel.AdminRights.PostMessages {
+				postMessages = "yes"
+			}
+			caser := cases.Title(language.English)
+			log.I("---------- ", caser.String(channelType), " channel info ----------")
+			log.I("Title:                ", channel.Title)
+			log.I("Can post messages:    ", postMessages)
+			log.I("------------------------------------------------")
+			break
+		} else {
+			log.E("Couldn't get info about ", channelType, " channel: ", err.Error())
+			break
+		}
+	}
 }
