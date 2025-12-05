40a22335d8ab2e216398e22da99fdeebf25b1bb4
dani3l0 <daniel.jank@protonmail.com>
2025-12-05 03:05:41 +0100

    use last stream in debug case as well

--- providers/rumble/getInfo.go
+++ providers/rumble/getInfo.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"nptw/config"
 	"nptw/utils/log"
 	"os/exec"
 )
@@ -15,11 +14,7 @@ var ErrPlaylistEmpty = errors.New("playlist empty")
 // Gets JSON-ed information about channel and last two replays
 func GetInfo(url string) (YtDlpResponse, error) {
 	var obj YtDlpResponse
-	playlistId := "1"
-	if config.Get().DebugReplays {
-		playlistId = "2"
-	}
-	c := exec.Command("./bin/yt-dlp", "-J", "-I", playlistId, url)
+	c := exec.Command("./bin/yt-dlp", "-J", "-I", "1", url)
 	output, errA := c.CombinedOutput()
 	errB := json.Unmarshal(output, &obj)
 	err := errors.Join(errA, errB)
