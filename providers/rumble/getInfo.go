823bacce5a0d3752088de707c50786282c03b32a
dani3l0 <daniel.jank@protonmail.com>
2025-12-10 02:01:07 +0100

    rumble scraper: Pull harder

--- providers/rumble/getInfo.go
+++ providers/rumble/getInfo.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"nptw/utils/log"
 	"os/exec"
+	"time"
 )
 
 // Empty list err -___-
@@ -14,17 +15,26 @@ var ErrPlaylistEmpty = errors.New("playlist empty")
 // Gets JSON-ed information about channel and last two replays
 func GetInfo(url string) (YtDlpResponse, error) {
 	var obj YtDlpResponse
-	c := exec.Command("./bin/yt-dlp", "-J", "-I", "1", url)
-	output, errA := c.CombinedOutput()
-	errB := json.Unmarshal(output, &obj)
-	err := errors.Join(errA, errB)
-	if err != nil {
-		log.E("Couldn't get channel info: ", err.Error())
-		log.E(string(output))
-	}
-	log.V(fmt.Sprintf("%+v", obj))
-	if len(obj.Entries) == 0 {
-		err = ErrPlaylistEmpty
+	var err error
+	for range 3 {
+		c := exec.Command("./bin/yt-dlp", "-J", "-I", "1", url)
+		output, errA := c.CombinedOutput()
+		errB := json.Unmarshal(output, &obj)
+		err = errors.Join(errA, errB)
+		if err != nil {
+			log.E("Couldn't get channel info: ", err.Error())
+			log.E(string(output))
+		}
+		log.V(fmt.Sprintf("%+v", obj))
+		if len(obj.Entries) == 0 {
+			err = ErrPlaylistEmpty
+		}
+		if err == nil {
+			break
+		} else {
+			log.E("rumble.GetInfo error: ", err.Error())
+			time.Sleep(time.Second * 15)
+		}
 	}
 	return obj, err
 }
